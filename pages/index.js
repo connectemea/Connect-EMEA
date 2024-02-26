b24c81523560eb41f148f9912ac97919d6cb9aec
dilshad360 <dilshad360.dev@gmail.com>
2024-02-26 14:41:01 +0530

    feat: font & bg color

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 // import Image from "next/image";
-import { Inter } from "next/font/google";
+import { HeroSection } from "@/components/HeroSection";
+import { NavBar } from "@/components/NavBar";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({ weight: ['400','500','700'], subsets: ["latin"] });
 
 export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex  flex-col items-center justify-between ${roboto.className}`}
     >
-      <video autoPlay loop muted className="object-cover w-full h-full fixed inset-0 z-[-1]">
-        <source src="/black.mp4" type="video/mp4" />
-      </video>
-      <div className="p-4 rounded-md shadow-md m-auto bg-white">
-       <h3 className="text-xl font-bold">Connect New Website</h3>
-      </div>
+      <NavBar/>
+      <HeroSection/>
     </main>
   );
 }
