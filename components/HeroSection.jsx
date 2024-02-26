907b01383b1b8dc615ec68d88a4c0883e2e186c7
dilshad360 <dilshad360.dev@gmail.com>
2024-02-26 14:41:01 +0530

    feat: font & bg color

---
+++ components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const HeroSection = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center" >
+            <div className="p-4 rounded-md shadow-md m-auto ">
+                Hero Section
+                <h3 className="text-xl font-bold">Connect New Website</h3>
+            </div>
+        </div>
+    );
+};
