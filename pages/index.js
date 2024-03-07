8576526b9d3a4a4593201c18fdc2d28d1820ccb7
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-03-08 00:17:07 +0530

    Add: Ui implemented

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 // import Image from "next/image";
-import { HeroSection } from "@/components/HeroSection";
-import { NavBar } from "@/components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { NavBar } from "../components/NavBar";
+import { MacbookScrollDemo } from "../components/Macbook.tsx";
+import {BentoGridDemo} from "../components/Text.tsx";
 import { Roboto } from "next/font/google";
 
 
@@ -9,11 +11,9 @@ const roboto = Roboto({ weight: ['400','500','700'], subsets: ["latin"] });
 export default function Home() {
 
   return (
-    <main
-      className={`flex  flex-col items-center justify-between ${roboto.className}`}
-    >
-      <NavBar/>
-      <HeroSection/>
+    <main>
+      <MacbookScrollDemo />
+      <BentoGridDemo />
     </main>
   );
 }
