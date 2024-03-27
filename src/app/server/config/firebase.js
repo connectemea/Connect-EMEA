e992fd46f2c767f693ca5a56beb00f4f06c02715
Muhammed Shamil KP <130832663+muhammedshamil8@users.noreply.github.com>
2024-03-27 23:11:56 +0530

    #

--- src/app/server/config/firebase.js
+++ src/app/server/config/firebase.js
@@ -3,14 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBxFxojqLf6tlgfQvehE1RNExCPrEszuCw",
-  authDomain: "connect-portfolio.firebaseapp.com",
-  projectId: "connect-portfolio",
-  storageBucket: "connect-portfolio.appspot.com",
-  messagingSenderId: "308934822913",
-  appId: "1:308934822913:web:c4c7ff75acc7a7dcbd0400"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
