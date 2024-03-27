f643cb8a9d879e5e3f1a7c4a9fbb035c6c97077b
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-03-27 23:14:19 +0530

    Updated: ..

--- src/app/server/config/firebase.js
+++ src/app/server/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxFxojqLf6tlgfQvehE1RNExCPrEszuCw",
-  authDomain: "connect-portfolio.firebaseapp.com",
-  projectId: "connect-portfolio",
-  storageBucket: "connect-portfolio.appspot.com",
-  messagingSenderId: "308934822913",
-  appId: "1:308934822913:web:c4c7ff75acc7a7dcbd0400"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
